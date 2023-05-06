bc3ef201c91ff2ae8375cdf6c9aa75eff005c580
mufazmi <info.mufazmi@gmail.com>
2023-05-06 13:10:26 +0530

    Implemented : Side Navigation & Bug Fixes

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ import store from './redux/store';
 import { Provider } from 'react-redux';
 import { Toaster } from 'react-hot-toast';
 
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
